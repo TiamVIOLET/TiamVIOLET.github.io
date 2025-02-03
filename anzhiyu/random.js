a907e2646449d3bab43df809f9499be70cbd5458
gaoxinqi <abc@qq.com>
2025-02-03 16:39:46 +0800

    Site updated: 2025-02-03 16:39:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/01/hello-world/","2025/02/01/1/"];function toRandomPost(){
+var posts=["2025/02/01/hello-world/","2024/01/01/1/","2025/02/03/2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
